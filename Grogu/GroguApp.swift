5c08cdb978a130200fb0506abde359682b9da603
Jacek Kosiński <kashiash@gmail.com>
2023-04-19 16:49:09 +0200

    Initial Commit

---
+++ Grogu/GroguApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroguApp.swift
+//  Grogu
+//
+//  Created by Jacek Kosinski U on 19/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GroguApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
